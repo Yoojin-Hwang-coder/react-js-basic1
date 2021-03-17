937b379b2a732c799cfb66749f1d73e0e0bc1b3f
Yoojin Hwang <dbwlshyj89@gmail.com>
2021-03-17 13:52:22 +0900

    로컬스토리지, 토큰 기능 추가

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import LandingPage from './component/view/LandingPage/LandingPage';
+import LoginPage from './component/view/LoginPage/LoginPage';
+import RegisterPage from './component/view/RegisterPage/RegisterPage';
+import './App.css';
+import Auth from './HOC/auth';
+import Navbar from './component/view/Navbar/Navbar';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div>
+        <Switch>
+          <Route exact path='/' component={Auth(LandingPage, null)} />
+          <Route exact path='/login' component={Auth(LoginPage, false)} />
+          <Route exact path='/register' component={Auth(RegisterPage, false)} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
